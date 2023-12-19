77a77518d26cc58067b2e70d17a608939ca1e713
leo hjulström <leo.hjulstrom@hotmail.com>
2023-12-19 18:08:02 +0100

    second commit

--- guessGame.py
+++ guessGame.py
@@ -18,9 +18,26 @@ ORDER BY RAND()
 LIMIT 1
 """
 
+def getWeightOf(pokemon):
+    query = """
+    SELECT ?object
+    WHERE {
+        GRAPH <https://pokemonkg.org/dataset/bulbapedia> {
+            ?subject ?predicate ?object .
+            FILTER (?subject = <https://pokemonkg.org/instance/pokemon/"""+pokemon+"""/weight/quantity>)
+            FILTER (?predicate = <http://qudt.org/schema/qudt/quantityValue>)
+        }
+    }
+    """
+
+    result = dataset.query(query)
+    return str(result.bindings).split("value/")[1].split("'")[0].lower()
+
 results = dataset.query(query)
 pokemon = str(results.bindings[0]).split("Literal('")[1].split("'")[0]
+weight = getWeightOf(pokemon.lower())
 
+print("The weight of the pokemon is " + weight)
 print("Guess a pokemon: ")
 hiddenWord = pokemon[:3]
 for _ in range(len(pokemon[3:])):
